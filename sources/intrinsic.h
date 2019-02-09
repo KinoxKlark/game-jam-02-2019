33b68962b3e0fee51540f1d0b2bd37f746975860
David Gallay <davidtennis96@hotmail.com>
2019-02-09 14:55:14 +0100

    intrinsic.h : using stdint.h for types

--- sources/intrinsic.h
+++ sources/intrinsic.h
@@ -1,8 +1,6 @@
 #ifndef INTRINSIC_H
 #define INTRINSIC_H
 
-#ifdef WIN32
-
 #include <stdint.h>
 
 typedef float r32;
@@ -17,22 +15,5 @@ typedef int8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-
-#else
-
-typedef float r32;
-typedef double r64;
-
-typedef char i8;
-typedef short int i16;
-typedef int i32;
-typedef long int i64;
-
-typedef unsigned char  u8;
-typedef unsigned short int u16;
-typedef unsigned int u32;
-typedef unsigned long int u64;
-
-#endif
 
 #endif
