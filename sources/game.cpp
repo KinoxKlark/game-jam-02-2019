2ab197a4bcd985dcb572566690f4a9db62fbba8b
Samuel Ernst <samuel.ernst.09@gmail.com>
2019-02-10 21:37:46 +0100

    systeme d'unite

--- sources/game.cpp
+++ sources/game.cpp
@@ -11,22 +11,22 @@ void game_init(GameData& data)
 	Weapon player_weapon;
 	player_weapon.type = GT_pistol;
 	player_weapon.projectile_type = PT_pistol_bullet;
-	player_weapon.cooldown = 0.2;
+	player_weapon.cooldown = 0.2; // sec
 	player_weapon.used = false;
 	player_weapon.waited_time = 0;
 	
 	// Player
 	data.player.to_destroy = false;
-	data.player.pos.x = 300;
-	data.player.pos.y = 200;
-	data.player.max_speed = 1000.f;
-	data.player.acceleration = 1000.f;
+	data.player.pos.x = 3; // units
+	data.player.pos.y = 2; // units
+	data.player.max_speed = 10.f;    // units / sec
+	data.player.acceleration = 10.f; // units / sec^2
 	data.player.masse = 60.f; // 60kg
-	data.player.collision_radius = 45.f;
+	data.player.collision_radius = .45f; // units
 	data.player.tp_charge = 0;
-	data.player.tp_charging_speed = 300;
-	data.player.tp_max_distance = 500;
-	data.player.orientation.x = 0;
+	data.player.tp_charging_speed = 3; // units / sec
+	data.player.tp_max_distance = 5;   // units
+	data.player.orientation.x = 0; // Unitlength vector
 	data.player.orientation.y = 1;
 	data.player.weapon = player_weapon;
 	data.player.asset_type = AssetType::PLAYER;
@@ -47,12 +47,12 @@ void game_init(GameData& data)
 	for(size_t i(0); i < 3; i++)
 	{
 		Entity e;
-		e.pos.x = data.player.pos.x + (i+1) * 100;
+		e.pos.x = data.player.pos.x + (i+1) * 1;
 		e.pos.y = data.player.pos.y;
-		e.max_speed = 500.f;
-		e.acceleration = 100.f * i;
+		e.max_speed = 5.f;
+		e.acceleration = 1.f * i;
 		e.masse = 60.f;// 60kg
-		e.collision_radius = 45.f;
+		e.collision_radius = .45f;
 		e.life_max = 5;
 		e.life = 5;
 
@@ -68,13 +68,13 @@ void game_init(GameData& data)
 
 void game_tick(GameData& data, Inputs& inputs)
 {
-	r32 friction(0.5f);//Can depend on the floor
+	r32 friction(0.5f); //Can depend on the floor
 	r32 world_delta_time = data.time_factor*inputs.delta_time;
 	
 	// Slow motion
 	// TODO(Sam): Transition plus smooth
 	// C'est pour ce genre de test que le nom charging_tp n'est pas
-	// du tout adapt�, ici ce qu'on veut dire c'est : si l'action
+	// du tout adapte, ici ce qu'on veut dire c'est : si l'action
 	// secondaire est en cours alors ralentir le temps
 	if(inputs.charging_tp == true)
 	{
@@ -129,9 +129,6 @@ void game_tick(GameData& data, Inputs& inputs)
 			{
 				// TODO(Sam): Que se passe-t-il lors de la collision ?
 				// Touche par un ennemi ...
-
-				//vector push_direction = safe_normalise(wp_from_entity);
-				//wanted_pos = entity.pos + push_direction*distance_between;
 			}
 		}
 #endif		
@@ -141,7 +138,7 @@ void game_tick(GameData& data, Inputs& inputs)
 	
 	//shooting
 	r32 direction2_length(norm(inputs.direction2));
-	if(direction2_length > 0.1) // TODO(Sam): Quelle sensibilit� ?
+	if(direction2_length > 0.1) // TODO(Sam): Quelle sensibilite ?
 		data.player.orientation = inputs.direction2 / direction2_length;
 
 	if(data.player.weapon.used)
@@ -162,7 +159,7 @@ void game_tick(GameData& data, Inputs& inputs)
 				Projectile p;
 				p.type = PT_pistol_bullet;
 				p.life_time = 4;
-				p.speed = 200;
+				p.speed = 2;
 				p.dommage = 1;
 				p.pos = data.player.pos;
 				p.direction = data.player.orientation;
@@ -178,15 +175,15 @@ void game_tick(GameData& data, Inputs& inputs)
 	while(data.ennemies.size() < 3)
 	{
 		r32 angle = (r32)(rand() % 360) / 360.f * PI2;
-		r32 distance = 1000.f;
+		r32 distance = 10.f;
 		
 	    Entity ennemi;
 		ennemi.pos.x = data.player.pos.x + distance * std::cos(angle);
 		ennemi.pos.y = data.player.pos.y + distance * std::sin(angle);
-		ennemi.max_speed = 500.f;
-		ennemi.acceleration = 100.f;
+		ennemi.max_speed = 5.f;
+		ennemi.acceleration = 1.f;
 		ennemi.masse = 60.f; // 60kg
-		ennemi.collision_radius = 45.f;
+		ennemi.collision_radius = .4f;
 		ennemi.life_max = 5;
 		ennemi.life = 5;
 
@@ -196,7 +193,6 @@ void game_tick(GameData& data, Inputs& inputs)
 	}
 	for(auto& ennemi : data.ennemies)
 	{
-		// TODO(Sam): C'est bien le bon endroit pour faire ca ?
 		if(ennemi.life <= 0.f)
 		{
 			ennemi.to_destroy = true;
@@ -240,7 +236,6 @@ void game_tick(GameData& data, Inputs& inputs)
 			}
 			// TODO(Sam): Il faut merger les deux, les collisions devrait etre testee
 			// sur les entites et pas seulement les ennemis...
-			if(true)
 			{
 				vector wp_from_entity = wanted_pos - data.player.pos;
 				r32 distance_final = norm(wp_from_entity);
@@ -293,8 +288,9 @@ void game_tick(GameData& data, Inputs& inputs)
 		// Camera focus
 	    r32 direction2_length(norm(inputs.direction2));
 		r32 speed_factor(.1f);
+		r32 distance_upfront(1.f);
 		vector marker_1(
-			data.player.pos+data.player.orientation*100.f*direction2_length
+			data.player.pos+data.player.orientation*distance_upfront*direction2_length
 			+ data.player.speed*speed_factor);
 
 		vector marker_2(marker_1);
